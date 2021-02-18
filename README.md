021e8088ffcb2326ed4926db6d5c019e682b401b
reselection <danieldcwverlinden@gmail.com>
2021-02-18 22:24:10 +0100

    added some documentation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # asset-tracker
 <p>With the use of an API this program will track requested stocks and some crypto.</p>
+I want for the finished product that a user can request things from a set of items provided by the program.
+For now i won't add GUI functionality and keep it in the terminal.
+
--- tracker.py
+++ tracker.py
@@ -8,13 +8,13 @@ import requests
 asset = input("Enter asset ticker: ")
 
 def asset_request(url):
-    """
-    requests json data
-    """
+    """requests json data and returns the price"""
     company_data  = requests.get(url)
     company_data = company_data.json()
-    price = float(f"{company_data[0]['price']}")
-    return price
+    print("What data would you like to recieve back?i\nEnter 'help' for more info.")
+    reqdata = input(": ")
+    price = f"{company_data[0]['price']}"
+    return float(price)
 
 url = (f"https://financialmodelingprep.com/api/v3/quote/{asset.upper()}?apikey=2bdce558ad8fcab19ca4cbd5abf8a21b")
 
