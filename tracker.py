cc20112643ce2208c83792554405dcaf737d6d85
reselection <danieldcwverlinden@gmail.com>
2021-02-18 18:14:13 +0100

    current set it so it only returns the price

--- tracker.py
+++ tracker.py
@@ -1,10 +1,23 @@
 #!/bin/python3
 
-import requests
 import pandas as pd
+import json
+from urllib.request import urlopen
+import requests
 
-api = '2bdce558ad8fcab19ca4cbd5abf8a21b'
-search = input('Enter a stock or crypto: ')
-url = f'https://financialmodelingprep.com/api/v3/profile/{search}?apikey={api}'
+asset = input("Enter asset ticker: ")
 
-print(url)
+def asset_request(url):
+    """
+    requests json data
+    """
+    company_data  = requests.get(url)
+    company_data = company_data.json()
+    price = float(f"{company_data[0]['price']}")
+    return price
+
+url = (f"https://financialmodelingprep.com/api/v3/quote/{asset.upper()}?apikey=2bdce558ad8fcab19ca4cbd5abf8a21b")
+
+print(asset_request(url))
+
+
